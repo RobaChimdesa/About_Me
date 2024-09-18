d006ed817a7d51fef3d93257adb73a71d77e0f62
Roba Chimdesa <130156480+RobaChimdesa@users.noreply.github.com>
2024-09-18 12:33:02 +0300

    Update certificate.jsx

--- src/constants/certificate.jsx
+++ src/constants/certificate.jsx
@@ -6,6 +6,9 @@ import { figma } from "../assets"
 import { rect } from "../assets"
 import {datascience } from "../assets"
 import { DBMS } from "../assets"
+import { Django } from "../assets"
+import { linux } from "../assets"
+
 export const Certificates = [
     {
         id:'10',
@@ -36,6 +39,16 @@ export const Certificates = [
         id:'50',
         img:DBMS,
         name:"DBMS"
+    },
+    {
+        id:'60',
+        img:Django,
+        name:"python, django and html5"
+    },
+    {
+        id:'70',
+        img:linux,
+        name:"Linux Command"
     }
      
 ]
