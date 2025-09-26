5e56856fcf52e32c8616165572b078bd7bae19a2
Roba Chimdesa <130156480+RobaChimdesa@users.noreply.github.com>
2025-09-26 13:53:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,22 @@ import Skill from './Components/Skill/Skill';
 import Contact from './Components/Contact/contact';
 import Project from './Components/Project/project';
 import Footer from './Components/footer/Footer';
+import { lbg } from './assets';
 
 
 
 const App = () => {
   return (
    
-    <div className="w-full h-auto   bg-zinc-950 text-white  font-serif">
+    <div className="w-full h-auto   bg-zinc-950 text-white  font-serif"
+       style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(${lbg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+      }}
+      >
         
       
        <div className='max-w-screen-xl px-1  md:px-80  p-10 md:text-xl  '>
