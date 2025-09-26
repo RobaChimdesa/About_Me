1b9a47e9fb9f95c1cf04d98faf3deaf8e4a7908c
Roba Chimdesa <130156480+RobaChimdesa@users.noreply.github.com>
2025-09-26 14:46:24 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,53 @@
+// import React from 'react';
+// import Navbar from './Components/Navbar/Navbar';
+// import HomePage from './Components/HomePage/HomePage';
+// import About from './Components/About/About';
+// import Education from './Components/Education/Education';
+// import Skill from './Components/Skill/Skill';
+// import Contact from './Components/Contact/contact';
+// import Project from './Components/Project/project';
+// import Footer from './Components/footer/Footer';
+// import { lbg } from './assets';
+
+
+
+// const App = () => {
+//   return (
+   
+//     <div className="w-full h-auto   bg-zinc-950 text-white  font-serif"
+//        style={{
+//         backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(${lbg})`,
+//         backgroundSize: "cover",
+//         backgroundPosition: "center",
+//         backgroundRepeat: "no-repeat",
+//         minHeight: "100vh",
+//       }}
+//       >
+        
+      
+//        <div className='max-w-screen-xl px-1  md:px-80  p-10 md:text-xl  '>
+//          <Navbar/>
+        
+//           </div>
+//           <HomePage/>
+//             <About/>
+//          <Education/>
+//            <Skill/>
+//           <Project/>
+//           {/* <Cat/> */}
+//            <Contact/>
+//           <Footer/> 
+         
+        
+         
+//        </div>
+//   );
+// };
+
+
+
+// export default App; 
+
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import HomePage from './Components/HomePage/HomePage';
@@ -7,43 +57,34 @@ import Skill from './Components/Skill/Skill';
 import Contact from './Components/Contact/contact';
 import Project from './Components/Project/project';
 import Footer from './Components/footer/Footer';
+import { nav1 } from './assets';
 import { lbg } from './assets';
-
-
 
 const App = () => {
   return (
-   
-    <div className="w-full h-auto   bg-zinc-950 text-white  font-serif"
-       style={{
+    <div
+      className="w-full text-white font-serif"
+      style={{
         backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(${lbg})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
         minHeight: "100vh",
       }}
-      >
-        
-      
-       <div className='max-w-screen-xl px-1  md:px-80  p-10 md:text-xl  '>
-         <Navbar/>
-        
-          </div>
-          <HomePage/>
-            <About/>
-         <Education/>
-           <Skill/>
-          <Project/>
-          {/* <Cat/> */}
-           <Contact/>
-          <Footer/> 
-         
-        
-         
-       </div>
+    >
+      <div className="max-w-screen-xl px-1 md:px-80 p-10 md:text-xl mx-auto">
+        <Navbar />
+        <HomePage />
+        <About />
+        <Education />
+        <Skill />
+        <Project />
+        {/* <Cat/> */}
+        <Contact />
+        <Footer />
+      </div>
+    </div>
   );
 };
 
-
-
-export default App; 
+export default App;
